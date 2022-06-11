b52c51481903fe08f1f554f619740580e8459d4e
Daniel Ostermann <dostermann912@gmail.com>
2022-06-11 16:00:17 +0000

    version 0.1.0

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use std::io;
+use std::io::Write;
+use std::process;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+
+fn main() {
+    let mut length: String = String::new();
+
+    let gen_passwd = |length| -> String {
+    let password: String = thread_rng().sample_iter(Alphanumeric).take(length).collect();
+    password
+    };
+
+    println!("Welcome to password_generator {}!", VERSION);
+    println!("");
+
+    print!("Please enter the length of your new password: ");
+    io::stdout().flush().expect("Failed to flush the line!");
+
+    io::stdin()
+        .read_line(&mut length)
+        .expect("Failed to read the line!");
+
+    println!("");
+
+    let length: usize = match length.trim().parse() {
+        Ok(num) => num,
+        Err(_) => process::exit(1),
+    };
+
+    let new_password = gen_passwd(length);
+
+    println!("Your new password is:");
+    println!("");
+    println!("{}", new_password);
+    println!("");
+}
